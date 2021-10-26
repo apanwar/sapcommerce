5c6b3fa558b7e7e687e5927c15f45f418e3fbe1d
Swagatam Nanda <swagatam.nanda@publicissapient.com>
2021-10-26 17:22:48 +0530

    My First Commit

---
+++ hybris/bin/custom/trainingbackoffice/backoffice/src/org/training/backoffice/services/TrainingbackofficeService.java
@@ -0,0 +1,15 @@
+/*
+ * Copyright (c) 2020 SAP SE or an SAP affiliate company. All rights reserved
+ */
+package org.training.backoffice.services;
+
+/**
+ * Hello World TrainingbackofficeService
+ */
+public class TrainingbackofficeService
+{
+	public String getHello()
+	{
+		return "Hello";
+	}
+}
